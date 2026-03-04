b9dc750beeab4ba8aa979c60d957be7f340869e8
Sohee Kim <wneh2023@naver.com>
2026-03-05 06:35:26 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,16 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, num = 5000, mn = 5000, k;
+int n, cnt;
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-	cin >> n;
-    for (int i = n/5; i >= 0; i--) {
-        k = n-(i*5);
-        if(k % 3 == 0) num = k/3 + i;
-        mn = min(num, mn);
-    }
-    if (mn == 5000) cout << "-1\n";
-    else cout << mn << "\n";
-}
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
+    cin >> n;
+
+    while (n >= 0) {
+        if (n % 5 == 0) {
+            cnt += n / 5;
+            cout << cnt << "\n";
+            return 0;
+        }
+        n -= 3;
+        cnt++;
+    }
+
+    cout << -1 << "\n";
+    return 0;
+}
