4123a948e492bd90856be552f45e6dcf3c7d42d1
Sohee Kim <wneh2023@naver.com>
2026-03-05 23:00:01 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -1,22 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-int n, a[1004], dp[1004], ans;
-int solve(int n) {
-    for (int i = 1; i <= n; i++) {
-        dp[i] = 1;
-        for (int j = i - 1; j >= 1; j--) {
-            if (a[i] > a[j]) dp[i] = max(dp[i], dp[j]+1);
-        }
-        ans = max(dp[i], ans);
-    }
-    return ans;
-}
+int n, a[1004], dp[1004];
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(NULL); cout.tie(NULL);
-    cin >> n;
-	for (int i = 1; i <= n; i++) {
+	cin.tie(nullptr);
+	cin >> n;
+    for (int i = 1; i <= n; i++) {
         cin >> a[i];
     }
-    cout << solve(n) << "\n";
+    fill(dp, dp + 1004, 1);
+    for (int i = 2; i <= n; i++) {
+        for (int j = 1; j < i; j++) {
+            if (a[i] > a[j]) dp[i] = max(dp[i], dp[j] + 1);
+        }
+    }
+    int mx = -1;
+    for (int i = 1; i <= n; i++) {
+        mx = max(mx, dp[i]);
+    }
+    cout << mx << "\n";
 }
