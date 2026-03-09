a7d1f07c581690f1b96f2f746f9de28c8a5d3962
Sohee Kim <wneh2023@naver.com>
2026-03-10 02:59:09 +0900

    [Bronze II] Title: 단어의 개수, Time: 28 ms, Memory: 3680 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s; int cnt;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	while (cin >> s) {
+        cnt++;
+    }
+    cout << cnt << "\n";
+}
