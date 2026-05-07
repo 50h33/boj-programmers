187a4c51bc1ee5c8548cad69665d2c085e22d1ec
김소희 <wneh2023@naver.com>
2026-05-07 19:21:56 +0900

    [level 3] Title: 네트워크, Time: 1.38 ms, Memory: 6.31 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,23 +1,23 @@
-#include <string>
-#include <vector>
+#include <bits/stdc++.h>
 using namespace std;
 bool visited[204];
 
-void go(int cur, vector<vector<int>> computers) {
-    visited[cur] = true;
-    for (int nxt = 0; nxt < computers[cur].size(); nxt++) {
-        if (computers[cur][nxt] && !visited[nxt]) {
-            go(nxt, computers);
+void go(int idx, int n, vector<vector<int>> computers) {
+    visited[idx] = 1;
+    for (int i = 0; i < computers[idx].size(); i++) {
+        if (idx == i) continue;
+        if (!visited[i] && computers[idx][i]) {
+            go(i, n, computers);
         }
     }
 }
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
-    for (int i = 0; i < computers.size(); i++) {
-        if (visited[i]) continue;
-        go(i, computers);
-        answer++;
+    for (int i = 0; i < n; i++) {
+        if(!visited[i]) {
+            go(i, n, computers); answer++;
+        }
     }
     return answer;
 }
