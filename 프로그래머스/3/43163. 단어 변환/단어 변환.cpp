494444506fa0305c6a24f63e7db85cb967da1efc
김소희 <wneh2023@naver.com>
2026-05-10 14:13:09 +0900

    [level 3] Title: 단어 변환, Time: 0.01 ms, Memory: 4.07 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.cpp
@@ -1,29 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
-int num;
-string cur;
 bool visited[54];
 queue<pair<string, int>> q;
 
-bool canChange(string begin, string word){
-    int diff = 0;
-    
-    for (int i = 0; i < begin.size(); i++) {
-        if (begin[i] != word[i]) diff++;
-    }
-    
-    return diff == 1;
-}
-
 int solution(string begin, string target, vector<string> words) {
     q.push({begin, 0});
-    while (q.size()) {
+    while(q.size()) {
         auto [cur, num] = q.front(); q.pop();
         if (cur == target) return num;
-        for (int i = 0; i < words.size(); i++) {
-            if (canChange(cur, words[i]) && !visited[i]) {
-                visited[i] = 1;
-                q.push({words[i], num+1});
+        for (int idx = 0; idx < words.size(); idx++) {
+            int diff = 0;
+            for (int i = 0; i < words[idx].size(); i++) {
+                if (words[idx][i] != cur[i]) diff++;
+            }
+            if (diff == 1 && !visited[idx]) {
+                visited[idx] = 1;
+                q.push({words[idx], num + 1});
             }
         }
     }
