c2371d78c609f7f350d5982576fcc3c087d04978
Sohee Kim <wneh2023@naver.com>
2026-08-14 15:23:01 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 95.92 ms, Memory: 106 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer; int prev = -1;
+    for (int num : arr) {
+        if (prev == num) continue;
+        answer.push_back(num);
+        prev = num;
+    }
+    return answer;
+}
