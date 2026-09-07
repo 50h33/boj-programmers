b29ea3fea2b4a4859ca5a813d8b3a34e6824dbb2
Sohee Kim <wneh2023@naver.com>
2026-09-07 19:52:10 +0900

    [level 3] Title: N으로 표현, Time: 2.29 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int solution(int N, int number) {
+    unordered_set<int> dp[9];
+    int num = 0;
+    for (int i = 1; i <= 8; i++) {
+        num = num * 10 + N;
+        dp[i].insert(num);
+        for (int j = 1; j < i; j++) {
+            for (int a : dp[j]) {
+                for (int b : dp[i-j]) {
+                    dp[i].insert(a + b);
+                    dp[i].insert(a - b);
+                    dp[i].insert(a * b);
+                    if (b != 0) dp[i].insert(a / b);
+                }
+            }
+        }
+        if (dp[i].count(number)) return i;
+    }
+    return -1;
+}
